e24eaf95427406adefaa1675e62eae2a004ead8a
Yiming Chen <dsdshcym@gmail.com>
2021-06-25 20:39:47 +0800

    mix new objext

---
+++ lib/objext.ex
@@ -0,0 +1,18 @@
+defmodule Objext do
+  @moduledoc """
+  Documentation for `Objext`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Objext.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/objext_test.exs
@@ -0,0 +1,8 @@
+defmodule ObjextTest do
+  use ExUnit.Case
+  doctest Objext
+
+  test "greets the world" do
+    assert Objext.hello() == :world
+  end
+end
