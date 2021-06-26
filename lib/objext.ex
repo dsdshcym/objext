8f66a64ce2924ce4e989e5c76ea1a37807b94b38
Yiming Chen <dsdshcym@gmail.com>
2021-06-27 07:54:56 +0800

    rename interfaces -> implements, implement -> implementation_module
    - use interface specifically for Objext interfaces

--- lib/objext.ex
+++ lib/objext.ex
@@ -4,10 +4,12 @@ defmodule Objext do
   """
 
   defmacro __using__(opts) do
-    interfaces = Keyword.get(opts, :implements, [])
+    implements = Keyword.get(opts, :implements, [])
 
-    quote bind_quoted: [interfaces: interfaces] do
-      implement = __MODULE__
+    quote bind_quoted: [implements: implements] do
+      implementation_module = __MODULE__
+
+      interfaces = implements
 
       for interface <- interfaces do
         @behaviour interface.__interface__(:behaviour_module)
@@ -35,7 +37,7 @@ defmodule Objext do
             for {function, arity} <- protocol.__protocol__(:functions) do
               args = Macro.generate_unique_arguments(arity, __MODULE__)
 
-              defdelegate unquote(function)(unquote_splicing(args)), to: implement
+              defdelegate unquote(function)(unquote_splicing(args)), to: implementation_module
             end
           end
         end
