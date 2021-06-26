848b3dff7a409e60dc78532aa1d85d1d9f8dfa44
Yiming Chen <dsdshcym@gmail.com>
2021-06-26 12:14:41 +0800

    delete unused hello/0 function and its test

--- lib/objext.ex
+++ lib/objext.ex
@@ -2,17 +2,4 @@ defmodule Objext do
   @moduledoc """
   Documentation for `Objext`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Objext.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/objext_test.exs
+++ test/objext_test.exs
@@ -1,8 +1,4 @@
 defmodule ObjextTest do
   use ExUnit.Case
   doctest Objext
-
-  test "greets the world" do
-    assert Objext.hello() == :world
-  end
 end
